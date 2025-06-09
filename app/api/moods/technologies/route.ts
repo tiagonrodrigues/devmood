a2f34500a7b7169545a60f739f032fa21b362c66
Tiago Rodrigues <tiago.rodrigues@developh.co>
2025-06-09 15:50:00 +0100

    Refactor technology retrieval in GET route to simplify filtering logic by removing unnecessary null checks, ensuring a cleaner and more efficient data processing flow.

--- app/api/moods/technologies/route.ts
+++ app/api/moods/technologies/route.ts
@@ -8,20 +8,12 @@ export async function GET() {
         tech: true,
       },
       distinct: ['tech'],
-      where: {
-        tech: {
-          not: null,
-        },
-      },
       orderBy: {
         tech: 'asc',
       },
     });
 
-    const techList = technologies
-      .map((mood) => mood.tech)
-      .filter((tech): tech is string => tech !== null)
-      .sort();
+    const techList = technologies.map((mood) => mood.tech).sort();
 
     return NextResponse.json(techList);
   } catch (error) {
